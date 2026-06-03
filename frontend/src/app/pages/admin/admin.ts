d1b004b78763e5343cb4e26b1ef52f74eac8ae8c
Gul E Haram <guleharam7@gmail.com>
2026-06-03 20:04:51 +0500

    Add admin panel

---
+++ frontend/src/app/pages/admin/admin.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-admin',
+  standalone: true,
+  templateUrl: './admin.html',
+  styleUrls: ['./admin.css']
+})
+export class AdminComponent {
+
+  users: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.http.get('https://localhost:5001/api/users')
+      .subscribe((res: any) => {
+        this.users = res;
+      });
+  }
+
+  deleteUser(id: string) {
+    this.http.delete(`https://localhost:5001/api/users/${id}`)
+      .subscribe(() => {
+        this.loadUsers();
+      });
+  }
+}
