0536cedd7aeb248e49d9d21f18006e0350a59c9b
Gul E Haram <guleharam7@gmail.com>
2026-06-03 18:59:29 +0500

    Add signup page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login';
-import { Signup } from './pages/signup/signup';
+import { SignupComponent } from './pages/signup/signup';
 import { Dashboard } from './pages/dashboard/dashboard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'signup', component: Signup },
+  { path: 'signup', component: SignupComponent },
   { path: 'dashboard', component: Dashboard }
 ];
--- frontend/src/app/pages/signup/signup.ts
+++ frontend/src/app/pages/signup/signup.ts
@@ -1,9 +1,45 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.html',
-  styleUrl: './signup.css',
+  styleUrls: ['./signup.css']
 })
-export class Signup {}
+export class SignupComponent {
+
+  signupForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.signupForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      role: ['Student']
+    });
+  }
+
+  onSignup() {
+    if (this.signupForm.invalid) return;
+
+    this.auth.register(this.signupForm.value).subscribe({
+      next: () => {
+        alert('User registered successfully');
+        this.router.navigate(['/login']);
+      },
+      error: () => {
+        alert('Signup failed');
+      }
+    });
+  }
+}
