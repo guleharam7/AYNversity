00ad20b02330a5fefffac7b0eccfc04085f77b18
Gul E Haram <guleharam7@gmail.com>
2026-06-03 16:34:00 +0500

    Initial ASP.NET Core Web API Setup

---
+++ AYNversitybackend/AYNversityAPI/AYNversityAPI/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace AYNversityAPI.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+
+        public string Role { get; set; } = "Student";
+    }
+}
