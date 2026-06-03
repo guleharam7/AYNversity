718207336a595128feb2d8288da9bc8901fbb240
Gul E Haram <guleharam7@gmail.com>
2026-06-03 17:07:59 +0500

    API setup fixed and gitignore added

--- AYNversitybackend/AYNversityAPI/AYNversityAPI/Program.cs
+++ AYNversitybackend/AYNversityAPI/AYNversityAPI/Program.cs
@@ -1,17 +1,26 @@
+using AYNversityAPI.Data;
+using AYNversityAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Controllers
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// MongoDB
+builder.Services.Configure<MongoDbSettings>(
+    builder.Configuration.GetSection("MongoDbSettings"));
+
+builder.Services.AddSingleton<UserService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
---
+++ AYNversitybackend/AYNversityAPI/AYNversityAPI/Services/UserService.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Driver;
+using AYNversityAPI.Models;
+using AYNversityAPI.Data;
+using Microsoft.Extensions.Options;
+
+namespace AYNversityAPI.Services
+{
+    public class UserService
+    {
+        private readonly IMongoCollection<User> _usersCollection;
+
+        public UserService(IOptions<MongoDbSettings> settings)
+        {
+            var mongoClient = new MongoClient(settings.Value.ConnectionString);
+
+            var mongoDatabase =
+                mongoClient.GetDatabase(settings.Value.DatabaseName);
+
+            _usersCollection =
+                mongoDatabase.GetCollection<User>("Users");
+        }
+
+        public async Task<List<User>> GetAsync() =>
+            await _usersCollection.Find(_ => true).ToListAsync();
+
+        public async Task CreateAsync(User user) =>
+            await _usersCollection.InsertOneAsync(user);
+    }
+}
